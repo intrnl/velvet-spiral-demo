5046e1d5e92c9162419b9a20dfafa6ac8cea4201
intrnl <intrnl28+git@gmail.com>
2022-09-20 22:41:02 +0700

    feat: spiral demo

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
-import App from './App.velvet';
+import Spiral from './components/Spiral.velvet';
+import Cursor from './components/Cursor.velvet';
+
 import './style.css';
 
 
-const app = new App();
-app.name = 'world';
+patchComponent(Spiral);
+patchComponent(Cursor);
 
-document.body.appendChild(app);
+const spiral = new Spiral();
+document.body.appendChild(spiral);
+
+
+function patchComponent (Component) {
+	// we don't want shadow DOM to be in our way here, make Velvet think we
+	// already have a shadow root in place.
+	Component.prototype.attachShadow = function () {
+		if (!this.shadowRoot) {
+			Object.defineProperty(this, 'shadowRoot', { value: this })
+		}
+
+		return this;
+	};
+}
